2a04e0bf39f57d10eb545d95f207f49e7d490f8a
minseok <imuchgabis@gmail.com>
2023-05-26 12:37:34 +0900

    first commit

--- back/Model/Slider.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const SliderSchema = new mongoose.Schema({
-  url: String,
-});
-
-const Slider = mongoose.model("Slider", SliderSchema);
-
-module.exports = { Slider };
--- front/src/components/Announcement.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Announcement.css";
-
-const Announcement = () => {
-  return <div className="announcement">민석마켓에 와주셔서 감사합니다!!</div>;
-};
-
-export default Announcement;
